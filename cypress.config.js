0705eef29fc304f7dc3ff9dcb980672b60730336
DeveAlbert <albertemmanuel29@gmail.com>
2023-11-12 04:40:38 +0100

    updated cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "cfpn5d",
   e2e: {
     baseUrl: 'https://www.saucedemo.com/',
     defaultCommandTimeout: 30000,
     pageLoadTimeout: 120000,
     chromeWebSecurity: false,
-
+    projectId: "cfpn5d",
     viewportHeight: 900,
     viewportWidth: 1400,
     setupNodeEvents(on, config) {
